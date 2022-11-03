45fd79ad5c48d1f0fed1d2d72b8fa52df2e59cfd
kousarmohammed <kousarjahan006@gmail.com>
2022-11-02 23:19:51 -0700

    First commit

---
+++ corejava/src/org/tns/oopsconcepts/Inheritance.java
@@ -0,0 +1,12 @@
+package org.tns.oopsconcepts;
+
+public class Inheritance {
+
+	public static void main(String[] args) {
+		Withdraw ob=new Withdraw();
+		System.out.println("the total amount"+ob.total_savings);
+		System.out.println("the total amount"+ob.Withdraw);
+
+	}
+
+}
---
+++ corejava/src/org/tns/oopsconcepts/Multilevel.java
@@ -0,0 +1,14 @@
+package org.tns.oopsconcepts;
+
+public class Multilevel {
+
+	public static void main(String[] args) {
+		Withdraw ob1=new Withdraw ();
+		ob1.Display1();
+		ob1.Display2();
+		ob1.Display3();
+		
+
+	}
+
+}
